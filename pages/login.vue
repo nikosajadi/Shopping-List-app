bf202d351b53f1c81cb8273496aa12f6116da68c
nikosajadi <na.sajadii.29@gmail.com>
2024-02-28 14:10:11 +0100

    [feature]add family page

--- pages/login.vue
+++ pages/login.vue
@@ -9,12 +9,45 @@ definePageMeta({
     <div class="flex w-full text-center">
         <h1 class="pt-10 text-3xl w-full text-center font-extrabold leading-none">FAMILY <br>SH😍PPING</h1>
     </div>
-    <form class="w-full max-w-sm">
-        <div class="md:w-1/3">
+    <form class="form-group ">
+    <div class="md:w-2/3">
+        <input type="text" id="username" placeholder="Email" class="form-control">
+        <input type="password" id="username" placeholder="Password" class="form-control">
+        <input type="text" id="username" placeholder="Username" class="form-control">
+        <h3 class="mb-5 mt-5 text-ml  leading-none text-gray-400">Other users can add you to their family by searching for your username</h3>
+        <h3 class="mb-5 mt-5 text-ml  leading-none text-gray-400"> by searching for your username</h3>
+        <input type="text" id="username" placeholder="Display Name" class="form-control">
+        <button class="text-white mt-6 mb-3 w-full  text-white font-bold py-4 px-4 rounded-2xl bg-gradient-to-br from-pink-200 to-orange-400 ">
             
-        </div>
+  Sign Up
+</button>
+
+    </div>
+
+    
     </form>
         <NuxtLink to="/">back</NuxtLink>
     </div>
     
 </template>
+
+<style>
+.form-group {
+  @apply md:w-1/3 p-10 mt-8;
+  margin-bottom: 1rem;
+  align-items: center;
+ 
+}
+input[type="text"],
+input[type="password"],
+input[type="email"] {
+  width: 100%;
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 0.25rem;
+  @apply rounded-2xl my-3 px-3 py-4 ;
+  
+}
+
+</style>
