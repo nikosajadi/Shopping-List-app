0f4c90fc243cc7b8e4707716111295c909690042
nikosajadi <na.sajadii.29@gmail.com>
2024-03-02 12:11:15 +0100

    [feature]add row table for login page and register

--- pages/login.vue
+++ pages/login.vue
@@ -3,32 +3,71 @@ definePageMeta({
   layout: "starter",
 });
 
+const mode = ref("login");
+
 </script>
 <template>
-    <div>
-    <div class="flex w-full text-center">
+<div>
+  <div class="flex w-full text-center">
         <h1 class="pt-10 text-3xl w-full text-center font-extrabold leading-none">FAMILY <br>SH😍PPING</h1>
-    </div>
-    <form class="form-group ">
-    <div class="md:w-2/3">
-        <input type="text" id="username" placeholder="Email" class="form-control">
-        <input type="password" id="username" placeholder="Password" class="form-control">
-        <input type="text" id="username" placeholder="Username" class="form-control">
-        <h3 class="mb-5 mt-5 text-ml  leading-none text-gray-400">Other users can add you to their family by searching for your username</h3>
-        <h3 class="mb-5 mt-5 text-ml  leading-none text-gray-400"> by searching for your username</h3>
-        <input type="text" id="username" placeholder="Display Name" class="form-control">
-        <button class="text-white mt-6 mb-3 w-full  text-white font-bold py-4 px-4 rounded-2xl bg-gradient-to-br from-pink-200 to-orange-400 ">Sign Up</button>
+      </div>
 
-    </div>
 
-    
-    </form>
-        <NuxtLink to="/">back</NuxtLink>
-    </div>
+      <div>
+        <row class="inline-flex w-full justify-center  shadow-sm w-screen" role="group">
+          <span class="table-group" @click="mode ='login'  " :class="{'active' : mode == 'login'}">login</span>
+          <span class="table-group" @click="mode ='Register'" :class="{'active' : mode == 'Register'}">Register</span>
+        </row>
+
+      </div>
+
+      <div v-if="mode === 'login'" >
+        <form class="form-group">
+          <div class="md:w-2/3">
+            <input type="text" id="email" placeholder="Email" class="form-control">
+            <input type="password" id="password" placeholder="Password" class="form-control">
+            <button class="text-white mt-6 mb-3 w-full font-bold py-4 px-4 rounded-2xl bg-gradient-to-br from-pink-200 to-orange-400">Sign In</button>
+            
+          </div>
+        </form>
+        
+        <NuxtLink to="/">Back</NuxtLink>
+      </div>
+   
+ 
+      
+      <div v-else class="active">
+       
+<form class="form-group">
+  <div class="md:w-2/3">
+    <input type="text" id="email" placeholder="Email" class="form-control">
+    <input type="password" id="password" placeholder="Password" class="form-control">
+    <input type="text" id="username" placeholder="Username" class="form-control">
+    <h3 class="mb-5 mt-5 text-ml leading-none text-gray-400">Other users can add you to their family by searching for your username</h3>
+    <input type="text" id="displayName" placeholder="Display Name" class="form-control">
+    <button class="text-white mt-6 mb-3 w-full font-bold py-4 px-4 rounded-2xl bg-gradient-to-br from-pink-200 to-orange-400">Sign Up</button>
+  </div>
+</form>
+
+        <NuxtLink to="/">Back</NuxtLink>
+      </div>
+
+   
+  </div>
+
+
     
 </template>
 
 <style>
+.table-group {
+  @apply mt-9 px-10 py-4 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white;
+}
+
+.active {
+  border-bottom: green;
+  color: aliceblue;
+}
 .form-group {
   @apply md:w-1/3 p-10 mt-8;
   margin-bottom: 1rem;
@@ -48,3 +87,5 @@ input[type="email"] {
 }
 
 </style>
+
+
