ac32737b8f428cbd612961304868a238574ab00f
nikosajadi <na.sajadii.29@gmail.com>
2024-05-05 12:06:02 +0200

    [feature] Add shareList page

---
+++ pages/sharedlist.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div class= "bg-orange-100 w-full h-screen cursor-pointer ">
+        <div class="h-screen w-full flex flex-col pb-20 justify-center items-center">
+            <img src="/img/share.png" alt="sharedlist" class="w-56"/>
+            <p class=" FiraCode-SemiBold text-2xl " >Shared Shopping List</p>
+            <p class=" FiraCode-SemiBold text-base">Sharing Shoping List</p>
+        </div>
+        <Footer />
+    </div>
+  
+</template>
